8c10114c17d248f42aa0fafdeebef66dc7d4b4cb
juand <domingoortin.j@gmail.com>
2021-09-29 21:26:05 +0200

    Added view binding

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -18,11 +18,17 @@ package com.example.android.diceroller
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.android.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        //setContentView(R.layout.activity_main)
     }
 }
